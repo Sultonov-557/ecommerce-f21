5a68ec58bfa4824f3be549b76f26c9ad052741b1
gangster <mezarcisecret0305@gmail.com>
2024-01-08 15:29:34 +0500

    add

--- src/components/layout/Banner.jsx
+++ src/components/layout/Banner.jsx
@@ -1,39 +1,43 @@
-import React from 'react'
-import Banner from '../../image/banner.png'
+import bannerImage from "../../image/banner.png";
 function Banner() {
-  return (
-    <section className='banner'>
-        <div className="container">
-            <div className="banner-row">
-                <div className="banner-left">
-                    <ul className="banner-list">
-                        <li className='banner-list-item'>Automobiles</li>
-                        <li className='banner-list-item'>Clothes and wear</li>
-                        <li className='banner-list-item'>Home interiors</li>
-                        <li className='banner-list-item'>Computer and tech</li>
-                        <li className='banner-list-item'>Tools, equipments</li>
-                        <li className='banner-list-item'>Sports and outdoor</li>
-                        <li className='banner-list-item'>Animal and pets</li>
-                        <li className='banner-list-item'>Machinery tools</li>
-                        <li className='banner-list-item'>More category</li>
-                    </ul>
-                </div>
-                <div className="banner-center">
-                    <div className="banner-banner">
-                        <div className="banner-banner__info">
-                            <h3 className="banner-banner__info-toptitle">Latest trending</h3>
-                            <h2 className="banner-banner__info-title">Electronic items</h2>
-                            <button className="banner-banner__info-btn">Learn moe</button>
-                        </div>
-                        <div className="banner-banner__image">
-                            <img src={Banner} alt="" />
-                        </div>
-                    </div>
-                </div>
+  return (  
+    <section className="banner">
+      <div className="container">
+        <div className="banner-row">
+          <div className="banner-left">
+            <ul className="banner-list">
+              <li className="banner-list-item">Automobiles</li>
+              <li className="banner-list-item">Clothes and wear</li>
+              <li className="banner-list-item">Home interiors</li>
+              <li className="banner-list-item">Computer and tech</li>
+              <li className="banner-list-item">Tools, equipments</li>
+              <li className="banner-list-item">Sports and outdoor</li>
+              <li className="banner-list-item">Animal and pets</li>
+              <li className="banner-list-item">Machinery tools</li>
+              <li className="banner-list-item">More category</li>
+            </ul>
+          </div>
+          <div className="banner-center">
+            <div className="banner-banner">
+              <div className="banner-banner__info">
+                <h3 className="banner-banner__info-toptitle">
+                  Latest trending
+                </h3>
+                <h2 className="banner-banner__info-title">Electronic items</h2>
+                <button className="banner-banner__info-btn">Learn moe</button>
+              </div>
+              <div className="banner-banner__image">
+                <img src={bannerImage} alt="" />
+              </div>
             </div>
+          </div>
+          <div className="banner-right">
+            
+          </div>
         </div>
+      </div>
     </section>
-  )
+  );
 }
 
 export default Banner;
