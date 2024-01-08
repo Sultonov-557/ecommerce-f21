f5f1ed757344eb0029e6997061bdac0d99bb0bee
gangster <mezarcisecret0305@gmail.com>
2024-01-08 14:55:51 +0500

    add

---
+++ src/components/layout/Banner.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Banner from '../../images/banner.png'
+function Banner() {
+  return (
+    <section className='banner'>
+        <div className="container">
+            <div className="banner-row">
+                <div className="banner-left">
+                    <ul className="banner-list">
+                        <li className='banner-list-item'>Automobiles</li>
+                        <li className='banner-list-item'>Clothes and wear</li>
+                        <li className='banner-list-item'>Home interiors</li>
+                        <li className='banner-list-item'>Computer and tech</li>
+                        <li className='banner-list-item'>Tools, equipments</li>
+                        <li className='banner-list-item'>Sports and outdoor</li>
+                        <li className='banner-list-item'>Animal and pets</li>
+                        <li className='banner-list-item'>Machinery tools</li>
+                        <li className='banner-list-item'>More category</li>
+                    </ul>
+                </div>
+                <div className="banner-center">
+                    <div className="banner-banner">
+                        <div className="banner-banner__info">
+                            <h3 className="banner-banner__info-toptitle">Latest trending</h3>
+                            <h2 className="banner-banner__info-title">Electronic items</h2>
+                            <button className="banner-banner__info-btn">Learn moe</button>
+                        </div>
+                        <div className="banner-banner__image">
+                            <img src={Banner} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Banner;
--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Banner from './Banner'
 
 function Main() {
   return (
-    <div>Main</div>
-  )
+    <>
+      <Banner/>
+    </>
+)
 }
 
 export default Main
